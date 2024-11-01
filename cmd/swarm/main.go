428657c2fb3bf90fbf37d19612203ca8e1f3052f
TheTGKing <32723159+TheTGKing@users.noreply.github.com>
2024-11-01 18:13:05 -0400

    added parralel processing

--- cmd/swarm/main.go
+++ cmd/swarm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"swarm"
+	"time"
 
 	"github.com/anthdm/hollywood/actor"
 )
@@ -10,18 +11,21 @@ import (
 func main() {
 	if err := swarm.Run(
 		initialize,
-		swarm.WithSeed(0),
-		swarm.WithNumMsgs(100),
+		// swarm.WithSeed(0),
+		swarm.WithNumMsgs(1000),
 		swarm.WithMessages(&TestMsg{}),
+		swarm.WithParellel(10),
+		swarm.WithInterval(time.Nanosecond),
 	); err != nil {
 		log.Println(err)
 	}
 }
 
-func initialize(engine *actor.Engine) {
+func initialize(engine *actor.Engine) func() {
 	for range 10 {
 		engine.Spawn(testActorProducer(), "testActor")
 	}
+	return nil
 }
 
 type TestMsg struct {
@@ -46,6 +50,6 @@ func (a *testActor) Receive(act *actor.Context) {
 	switch act.Message().(type) {
 	case actor.Initialized, actor.Started, actor.Stopped:
 	default:
-		log.Printf("%T - %+v", act.Message(), act.Message())
+		// log.Printf("%T - %+v", act.Message(), act.Message())
 	}
 }
