2b624b5e0fc583e89971808ce36bd6b1e5b68cd0
TheTGKing <32723159+TheTGKing@users.noreply.github.com>
2024-10-31 22:33:15 -0400

    fixed memory leak

---
+++ cmd/swarm/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"swarm"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
+func main() {
+	if err := swarm.Run(
+		initialize,
+		swarm.WithSeed(0),
+		swarm.WithNumMsgs(100),
+		swarm.WithMessages(testMsg{}),
+	); err != nil {
+		log.Println(err)
+	}
+}
+
+func initialize(engine *actor.Engine) {
+	for range 10 {
+		engine.Spawn(testActorProducer(), "testActor")
+	}
+}
+
+type testMsg struct {
+	Str   string
+	Int   int
+	Uint  uint
+	Float float64
+	Bool  bool
+	Slice []int
+	Array [10]int
+}
+
+type testActor struct{}
+
+func testActorProducer() actor.Producer {
+	return func() actor.Receiver {
+		return &testActor{}
+	}
+}
+
+func (a *testActor) Receive(act *actor.Context) {
+	switch act.Message().(type) {
+	case actor.Initialized, actor.Started, actor.Stopped:
+	default:
+		log.Printf("%T - %+v", act.Message(), act.Message())
+	}
+}
